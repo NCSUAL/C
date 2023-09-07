e36f6a16ef780ac9a07de64014397b1090f75554
NCSUAL <ncsual@naver.com>
2023-09-07 15:12:29 +0900

    과제

---
+++ 과제-1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+static int scanf_int(){
+	int a;
+	scanf("%d",&a);
+	return a;
+};
+
+int main(){
+	int a1 = scanf_int();
+	for(int a=0; a<a1; a++){
+		for(int b=0; b<=a;b++){
+			printf(" *");
+		}
+		printf("\n");
+	}
+}
