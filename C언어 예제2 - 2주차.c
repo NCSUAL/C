6b8da7e205183a1a8288b6900ffb2251167d16f4
NCSUAL <ncsual@naver.com>
2023-09-14 14:36:37 +0900

    Update C언어 예제2 - 2주차.c

--- C언어 예제2 - 2주차.c
+++ C언어 예제2 - 2주차.c
@@ -9,7 +9,7 @@ int main(){
 	scanf("%c",&ch,sizeof(ch));
 	
 	printf("2.문자입력: ");
-	scanf("%s",&str,sizeof(str));
+	scanf("%s",str,sizeof(str));
 	
 	printf("입력한 문자 출력:%c\n",ch);
 	printf("입력한 문자열 출력: %s\n",str);
