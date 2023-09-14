7ad6942a3fb17183ca242a059a8ec2b682d683fc
NCSUAL <ncsual@naver.com>
2023-09-14 16:24:46 +0900

    Create C언어 실습7 - 2주차.c

---
+++ C언어 실습7 - 2주차.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+	int a= 2;
+		while(a<=9){
+			if(a!=5){
+				printf("%d´Ü\n",a);
+				for(int b=1; b<=9; b++){
+				printf("%d\n", b*a);	
+			}
+			}
+			else{
+				
+			}
+			a++;
+		}
+
+}
