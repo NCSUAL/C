7ad9e059b2e1f78251ca7fb5329c83ba46075d89
NCSUAL <ncsual@naver.com>
2023-10-12 16:23:50 +0900

    Update 포인터 과제.c

--- c언어 실습- 4주차/포인터 과제.c
+++ c언어 실습- 4주차/포인터 과제.c
@@ -10,9 +10,7 @@ void swap(int *a, int *b){
 	*a = *b;
 	*b = temp;
 }
-void sort(number){
-	//배열 받기 -변수 선언 
-	int* array = (int*)malloc(number * sizeof(int));
+void sort(int* array,int number){
 	//배열 받기 -반복문
 	printf("배열: "); 
 	for(int a=0; a<number; a++){
@@ -39,15 +37,22 @@ void sort(number){
 			index++;
 		}
 	}
-	printf("정렬된 배열: ");
-	for(int b=0; b<number; b++){
-		printf("%d ",array[b]);
-	} 
-	free(array);
 }
 int main(){
 	//배열크기 받기
 	printf("배열크기:");
 	int number = scanf_int();
-	sort(number);
+	
+	//배열 받기 -변수 선언 
+	int* array = (int*)malloc(number * sizeof(int));
+	
+	//정렬 함수
+	sort(array,number);
+	
+	//정렬된 배열
+	printf("정렬된 배열: ");
+	for(int b=0; b<number; b++){
+		printf("%d ",array[b]);
+	} 
+	free(array);
 }
