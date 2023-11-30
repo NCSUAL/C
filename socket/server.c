71ec2e6d3b9b6dc05c12f3ecb2b160817de2ded7
NCSUAL <ncsual@naver.com>
2023-11-30 14:29:37 +0900

    Update server.c

--- socket/server.c
+++ socket/server.c
@@ -92,7 +92,7 @@ int main(int argc, char *argv[]){
 	closesocket(Sock);
 	
 	//메세지 주기
-	char message[Buffer] = "ㅎㅇ";
+	char message[Buffer] = "hellohellohello";
 	send(client_socket,message , strlen(message) , 0); 
 	
 	}
