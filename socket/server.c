50e3b8df9d544a9d72c283f7cb509f16061d9f4b
NCSUAL <ncsual@naver.com>
2023-12-01 00:14:56 +0900

    Update server.c

--- socket/server.c
+++ socket/server.c
@@ -93,7 +93,7 @@ int main(int argc, char *argv[]){
 	
 	//规涝厘 -> 1 规积己 -> 0 
 	char number[2];
-	int Recv = recv(client_socket,number,sizeof(number),0);
+	int Recv = recv(client_socket,number,2,0);
 	number[Recv] = '\0';
 	printf("%s",number);
 	if(number[1] =='0'){
