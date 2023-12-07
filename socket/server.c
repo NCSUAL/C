e39113930993a98fca85e0aade2e57e8673f6cec
NCSUAL <ncsual@naver.com>
2023-12-07 15:51:31 +0900

    Update server.c

--- socket/server.c
+++ socket/server.c
@@ -176,8 +176,8 @@ for(;;){
 				int client_socket_data_result = recv(client_socket,(char * )client_socket_data,1056,0);
 				int enter_client_socket_data_result = recv(enter_client_socket,(char*)enter_client_socket_data,1056,0);
 				
-				send(client_socket,(char *)enter_client_socket_data,1057,0);
-				send(enter_client_socket,(char *)client_socket_data,1057,0);
+				send(client_socket,(char *)enter_client_socket_data,1056,0);
+				send(enter_client_socket,(char *)client_socket_data,1056,0);
 				
 			}
 			
